c6449a90f3f620fb215e96b213f3bd2b6dcd8b97
Kaze Wong <developer.kaze@gmail.com>
2023-03-15 23:26:26 +0800

    Update autosing.py
    Update the description of the check-in result

--- autosing.py
+++ autosing.py
@@ -55,11 +55,11 @@ def start(cookie, username):
         sing_day = re.search(r'<div class="tip_c">(.*?)</div>', user_info).group(1)
         log_info = "{}当前{}".format(sing_day, current_money)
         print(log_info)
-        send("签到结果", log_info)
+        send("福吧签到 成功：", log_info)
 
     except Exception as e:
-        print("签到失败，失败原因:"+str(e))
-        send("签到结果", str(e))
+        print("福吧签到失败，失败原因:"+str(e))
+        send("福吧签到失败：", str(e))
 
 
 def get_addr():
